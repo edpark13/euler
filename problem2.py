90e3cea700a4dd6a082ad38b7f9b7b2663198a35
Edward <edpark13@gmail.com>
2015-04-23 13:03:28 -0700

    solve problem 2 with only one loop

--- problem2.py
+++ problem2.py
@@ -1,10 +1,13 @@
 def evenFibNum():
     l = [1, 2]
     i = 2
+    sum = 0
     while True:
         num = l[-2] + l[-1]
         if num < 4000000:
             l.append(num)
+            if num % 2 == 0:
+                sum += num
         else:
             break
         i += 1
