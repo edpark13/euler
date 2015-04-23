cbaa7732543d2dd826562ed7bff468692c3703ed
Edward <edpark13@gmail.com>
2015-04-23 12:52:31 -0700

    found sum of all fib numbers

--- problem2.py
+++ problem2.py
@@ -1,12 +1,21 @@
 def evenFibNum():
     l = []
-    for i in xrange(10):
+    i = 0
+    while True:
+        i += 1
         try:
-            l.append(i + l[-1])
+            num = i + l[-1]
+            print num
+            if num < 4000000:
+                l.append(num)
+            else:
+                break
         except IndexError:
             l.append(i)
-
-    return l
+    sum = 0
+    for j in l:
+        sum += j
+    return sum
 
 if __name__ == '__main__':
     print evenFibNum()
