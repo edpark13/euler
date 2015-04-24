cdc8aff3fc65edb35102794e259c37b7982f8d88
Edward <edpark13@gmail.com>
2015-04-24 11:33:56 -0700

    looked at other solutions and made my solution more concise

--- problem4.py
+++ problem4.py
@@ -4,20 +4,13 @@ def largest_palindrome_product(digits):
 
     digits
     """
-    largest = 0
     for i in xrange(10**digits, 10**digits-10**(digits-1), -1):
         for j in xrange(10**digits, 10**digits-10**(digits-1), -1):
             product = i * j
+            print product, i, j
             s = str(product)
-            k = 0
-            while True:
-                if k == len(s)/2:
-                    if largest < product:
-                        return product, i, j
-                elif s[k] == s[len(s) - k - 1]:
-                    k += 1
-                else:
-                    break
+            if s == s[::-1]:
+                return product, i, j
 
 if __name__ == '__main__':
-    print largest_palindrome_product(6)
+    print largest_palindrome_product(3)
