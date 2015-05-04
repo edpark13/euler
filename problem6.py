3f4bbed4cc658417e2a058575ec35bb95524ed7d
Edward <edpark13@gmail.com>
2015-05-04 14:03:18 -0700

    add a little optimization for problems and doc strings

--- problem6.py
+++ problem6.py
@@ -1,7 +1,11 @@
 def sum_square_difference(n):
+    """
+    Find the difference between the sum of the numbers 1 to n squared and the
+    numbers 1 to n squared added
+    """
     square = 0
     sum = 0
-    for i in xrange(n + 1):
+    for i in xrange(1, n + 1):
         square += i**2
         sum += i
     return sum**2 - square
