388859b47e4c1a193d62d39e683d16a19ad82c1c
Edward <edpark13@gmail.com>
2015-05-04 13:52:57 -0700

    add a helper for problem 5 for next time I look at it

--- problem5.py
+++ problem5.py
@@ -2,14 +2,17 @@ def smallest_multiple(n):
     """
     Find the smallest number that is divisible by number 2 to n
     """
+    l = [(2, 2)]
     i = 2
     for k in xrange(2, n + 1):
         if i % k > 0:
             for j in xrange(2, n + 1):
                 if i * j % k == 0:
                     i *= j
+                    l.append((k, j))
                     break
+    print l
     return i
 
 if __name__ == '__main__':
-    print smallest_multiple(30)
+    print smallest_multiple(10)
