470a11135d1d49a5acd827e48e951fa05ace2f5f
Edward <edpark13@gmail.com>
2015-05-04 13:25:20 -0700

    modify problem 5 to find primes

--- problem5.py
+++ problem5.py
@@ -1,11 +1,22 @@
 def smallest_multiple(n):
-    i = 1
-    while True:
-        for j in xrange(n,9999999999,n):
-            if i % j != 0:
-                break
-            return i
-        i += 1
+    primes = []
+    for i in xrange(2, n):
+        print "i is " + str(i)
+        largest_prime = 0
+        for j in xrange(2, i):
+            print "j is " + str(j)
+            if i % j == 0: 
+                print str(j) + " goes into " + str(i) + " evenly"
+                largest_prime = j
+        if largest_prime == 0:
+            primes.append(i)
+        else:
+            primes.append(largest_prime)
+    print primes
+    result = 1
+    for prime in primes:
+        result *= prime
+    return result
 
 if __name__ == '__main__':
     print smallest_multiple(10)
