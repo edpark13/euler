4c8a769ff9a54bf8835fb7013eae6e758988b7bd
Edward <edpark13@gmail.com>
2015-05-04 14:00:33 -0700

    finish problem 6

--- problem6.py
+++ problem6.py
@@ -1,10 +1,10 @@
 def sum_square_difference(n):
     square = 0
     sum = 0
-    for i in xrange(n):
+    for i in xrange(n + 1):
         square += i**2
         sum += i
-    return square - i
+    return sum**2 - square
 
 if __name__ == '__main__':
-    sum_square_difference(10)
+    print sum_square_difference(100)
