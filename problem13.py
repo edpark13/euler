fba18b6fbcff208465c4df20ac72bb4da163a510
Edward <edpark13@gmail.com>
2015-05-06 15:50:51 -0700

    solve problem 13

--- problem13.py
+++ problem13.py
@@ -1,7 +1,12 @@
 def large_sum():
+    """
+    From a file get the sum of all the digits that are seperated by lines
+    """
     f = open("13number.txt", 'r')
-    number = f.read()
-    
+    sum = 0
+    for line in f.readlines():
+        sum += int(line)
+    return sum
 
 if __name__ == '__main__':
-    print large_sum()
+    print str(large_sum())[0:10] # print only the first 10 digits
