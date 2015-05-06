805563ba961fa5a6b749ca0bce21777fe4050ba1
Edward <edpark13@gmail.com>
2015-05-06 12:37:27 -0700

    solve problem 7

--- problem7.py
+++ problem7.py
@@ -1,15 +1,17 @@
 def find_prime(num):
     l = [2]
+    count = 1
     i = 3
-    while i < num:
+    while count != num:
         print i
         for j in xrange(2, i):
             if i % j == 0:
                 break
         else:
             l.append(i)
+            count += 1
         i += 2
     return l
 
 if __name__ == '__main__':
-    print find_prime(10)
+    print find_prime(10001)
