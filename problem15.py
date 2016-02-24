dca14875eadf5140d01660745df8e54c6f72d3e8
edpark13 <edpark13@gmail.com>
2016-02-24 11:17:35 -0800

    add a faster method for problem 5

--- problem15.py
+++ problem15.py
@@ -1,3 +1,5 @@
+import time
+
 def lattice_path(grid_size):
   if grid_size == [0, 0]:
     return 1
@@ -10,5 +12,22 @@ def lattice_path(grid_size):
     paths += lattice_path([grid_size[0], grid_size[1] - 1])
   return paths
 
+
+def route_num(cube_size):
+    L = [1] * cube_size
+    for i in range(cube_size):
+        # print "i is " + str(i)
+        for j in range(i):
+            # print "j is " + str(j)
+            L[j] = L[j]+L[j-1]
+            print "j is %s and L[j] is %s" % (j, L[j])
+        L[i] = 2 * L[i - 1]
+        print "i is %s and L[i] is %s" % (i, L[i])
+    return L[cube_size - 1]
+
 if __name__ == '__main__':
-  print lattice_path([2, 2])
+  # print lattice_path([2, 2])
+  start = time.time()
+  n = route_num(4)
+  elapsed = (time.time() - start)
+  print "%s found in %s seconds" % (n,elapsed)
